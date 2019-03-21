a5643ee2c5a1ceec00e13bdab8e250cae676b1b2
shap87 <shane87web@gmail.com>
2019-03-21 18:04:02 +0200

    #43 improved logic for getting a fresh tokens and error handling in the PO sync function

--- api/lojibackend/quickbooks_sync/views.py
+++ api/lojibackend/quickbooks_sync/views.py
@@ -85,9 +85,9 @@ def auth_redirect(request):
 
 @api_view(['POST'])
 def sync(request):
-    '''
+    """
     API route for synchronization with QB
-    '''
+    """
     return fetch_purchase_orders(request.user)
 
 
@@ -111,82 +111,93 @@ def test_qb_connect(request):
         'Authorization': auth_header,
         'Accept': 'application/json'
     }
-    r = requests.get(url, headers=headers)
-    tokens_used_message = 'old tokens were used'
-    if r.status_code >= 400:
-        print('debug: connection error, trying to get new tokens...')
-        url_lambda = settings.AWS_REFRESH_TOKENS_ENDPOINT
-        api_key = settings.AWS_API_KEY
-        headers_lambda = {
-            'x-api-key': api_key,
-        }
-        r1 = requests.get(url_lambda, headers=headers_lambda, params={'refresh_token': refresh_token})
-        if r1.status_code != 200:
-            return Response({'error': 'connection error with lambda function', 'data': r1.text}, status=400)
-        response = json.loads(r1.text)
-        print('debug: lambda response:\n{}'.format(json.dumps(response, indent=2)))
-        new_access_token = response.get('access_token')
-        new_refresh_token = response.get('refresh_token')
-        if new_access_token and new_refresh_token:
-            qb_user.access_token = new_access_token
-            qb_user.refresh_token = new_refresh_token
-            qb_user.save()
-        else:
-            return Response({'error': 'received incorrect tokens', 'data': r1.text}, status=400)
-        auth_header = 'Bearer {0}'.format(new_access_token)
-        headers['Authorization'] = auth_header
-        r = requests.get(url, headers=headers)
-        if r.status_code >= 400:
-            return Response({'error': 'connection error', 'data': r.text}, status=400)
-        tokens_used_message = 'new tokens were used'
+    qb_connect_res = qb_request(qb_user, refresh_token, url, headers)
+    if 'error' in qb_connect_res:
+        return Response({'error': qb_connect_res['error'],
+                         'data': qb_connect_res.get('data', '')},
+                        status=qb_connect_res['status'])
     try:
-        company_info = json.loads(r.text).get('CompanyInfo', {})
+        company_info = json.loads(qb_connect_res['data']).get('CompanyInfo', {})
     except ValueError:
         company_info = {}
     domain = company_info.get('domain', '')
     company_name = company_info.get('CompanyName', '')
     if not domain and not company_name:
-        message = 'an incorrect QB server response with code {}:{}'.format(r.status_code, r.text)
+        message = 'an incorrect QB server response (code {}):{}'.format(qb_connect_res['status'], qb_connect_res['data'])
         return Response({'error': message}, status=400)
     message = 'you are successfully connected to domain {} and company {}, {}'.format(domain, company_name,
-                                                                                      tokens_used_message)
-    print(message)
+                                                                                      qb_connect_res['success'])
+    print('debug: {}'.format(message))
     return Response({'success': message}, status=200)
 
 
+def qb_request(qb_user, refresh_token, url, headers):
+    response = requests.get(url, headers=headers)
+    tokens_used_message = 'old tokens were used'
+    if response.status_code >= 400:
+        url_lambda = settings.AWS_REFRESH_TOKENS_ENDPOINT
+        api_key = settings.AWS_API_KEY
+        headers_lambda = {
+            'x-api-key': api_key,
+        }
+        response_lambda = requests.get(url_lambda, headers=headers_lambda, params={'refresh_token': refresh_token})
+        if response_lambda.status_code != 200:
+            return {'error': 'connection error with lambda function', 'data': response_lambda.text, 'status': 400}
+        json_data = json.loads(response_lambda.text)
+        new_access_token = json_data.get('access_token')
+        new_refresh_token = json_data.get('refresh_token')
+        if new_access_token and new_refresh_token:
+            qb_user.access_token = new_access_token
+            qb_user.refresh_token = new_refresh_token
+            qb_user.save()
+        else:
+            return {'error': 'received incorrect tokens', 'data': response_lambda.text, 'status': 400}
+        auth_header = 'Bearer {0}'.format(new_access_token)
+        headers['Authorization'] = auth_header
+        response = requests.get(url, headers=headers)
+        if response.status_code >= 400:
+            return {'error': 'connection error', 'data': response.text, 'status': 400}
+        tokens_used_message = 'new tokens were used'
+    print('debug: qb request detected, {}'.format(tokens_used_message))
+    return {'success': tokens_used_message, 'status': 200, 'data': response.text}
+
+
 def fetch_purchase_orders(user):
-    '''
+    """
     Append to purchase orders models new items from QB database
-    '''
-    #get access_token for quick books
+    """
+    # get access_token for quick books
     try:
         qb_user = QuickBooksUser.objects.get(user=user)
     except QuickBooksUser.DoesNotExist:
-        return Response(message="SignUp with QuickBooks first",status=401)
-    access_token=qb_user.access_token
-    realm_id=qb_user.realm_id
+        return Response({'message': "SignUp with QuickBooks first"}, status=401)
+    access_token = qb_user.access_token
+    refresh_token = qb_user.refresh_token
+    realm_id = qb_user.realm_id
 
     # prepare API request
     base_url = settings.INTUIT_QBO_BASE_URL
-    select_statment = 'select * from PurchaseOrder'
+    select_statement = 'select * from PurchaseOrder'
     url = '{0}/v3/company/{1}/query?query={2}'.format(base_url,
                                                       realm_id,
-                                                      select_statment)
+                                                      select_statement)
     auth_header = 'Bearer {0}'.format(access_token)
     headers = {
         'Authorization': auth_header,
         'Accept': 'application/json'
     }
     # make API call
-    response = requests.get(url, headers=headers)
+    qb_connect_res = qb_request(qb_user, refresh_token, url, headers)
+    if 'success' in qb_connect_res:
+        # perform serialization
+        pos_content = json.loads(qb_connect_res['data'])
 
-    if(response.status_code == 200):
-        #perform serialization
-        pos_content = json.loads(response.content)
-
-        #iterate purchase orders
+        # iterate purchase orders
+        record_count = 0
+        record_created = 0
         for po in pos_content['QueryResponse']['PurchaseOrder']:
-            vendor,created = Vendor.objects.get_or_create(qb_id=po['VendorRef']['value'])
+            record_count += 1
+            vendor, created = Vendor.objects.get_or_create(qb_id=po['VendorRef']['value'])
             if created:
                 vendor.name = po['VendorRef']['name']
                 vendor.city = po['VendorAddr']['Line4'].split(',')[0]
@@ -195,10 +206,10 @@ def fetch_purchase_orders(user):
                 vendor.contact_name = po['VendorAddr']['Line1']
                 vendor.save()
 
-
-            purchase_order,created = PurchaseOrder.objects.get_or_create(qb_id = po['Id'],
-                                                                         vendor = vendor)
+            purchase_order, created = PurchaseOrder.objects.get_or_create(qb_id=po['Id'],
+                                                                          vendor=vendor)
             if created:
+                record_created += 1
                 purchase_order.vendor = vendor
                 purchase_order.company = user.company_id
                 purchase_order.time_created = po['MetaData']['CreateTime']
@@ -206,20 +217,26 @@ def fetch_purchase_orders(user):
                 purchase_order.status = 'OA'
                 purchase_order.save()
 
-                #if purchase order is new we filling info for it
+                # if purchase order is new we filling info for it
                 for line in po['Line']:
                     if line['DetailType'] == 'ItemBasedExpenseLineDetail':
-                        part,created = Part.objects.get_or_create(qb_id=line['ItemBasedExpenseLineDetail']['ItemRef']['value'])
-                        #creating items
+                        part, created = Part.objects.get_or_create(qb_id=line['ItemBasedExpenseLineDetail']['ItemRef']['value'])
+                        # creating items
                         if created:
                             part.name = line['ItemBasedExpenseLineDetail']['ItemRef']['name']
                             part.save()
-
                         poi = PurchaseOrderItem.objects.create(po=purchase_order,
                                                                part=part,
                                                                unit_price=line['ItemBasedExpenseLineDetail']['UnitPrice'],
                                                                qty=line['ItemBasedExpenseLineDetail']['Qty'])
-        return Response(status=response.status_code)
+        message = 'data successfully synchronized for {}/{} records'.format(record_count, record_created)
+        print('debug: {}'.format(message))
+        return Response({'success': message}, status=qb_connect_res['status'])
 
-    #if API call failed
-    return response
+    # if API call failed
+    print('debug: error [{}] with code:{}, text:{}'.format(qb_connect_res['error'],
+                                                           qb_connect_res['status'],
+                                                           qb_connect_res.get('data', '')))
+    return Response({'error': qb_connect_res['error'],
+                     'data': qb_connect_res.get('data', '')},
+                    status=qb_connect_res['status'])
