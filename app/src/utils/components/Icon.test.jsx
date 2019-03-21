988bead701623996759d2a5a94ad69d8d47c7678
shap87 <shane87web@gmail.com>
2019-03-21 21:40:41 +1000

    #13 updating unit test for the icon component

--- app/src/utils/components/Icon.test.jsx
+++ app/src/utils/components/Icon.test.jsx
@@ -1,37 +1,44 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import feather from 'feather-icons';
 
 import Icon from './Icon';
 
 describe('Icon component', () => {
   let wrapper;
+  let toSvgMock;
+  const defaultProps = {
+    iconName: 'file',
+  };
 
   beforeEach(() => {
-    wrapper = shallow(<Icon iconName="plus" />);
+    toSvgMock = jest.spyOn(feather.icons[defaultProps.iconName], 'toSvg');
+    wrapper = shallow(<Icon {...defaultProps} />);
   });
 
-  it('should render fontawesome icon', () => {
-    expect(wrapper).toMatchSelector('i');
-    expect(wrapper).toHaveClassName('fa fa-plus');
+  afterEach(() => {
+    toSvgMock.mockRestore();
   });
 
-  it('should render regular fontawesome icon', () => {
-    wrapper.setProps({ type: 'regular' });
-    expect(wrapper).toHaveClassName('far');
+  it('should render st-icon with default size', () => {
+    expect(wrapper).toHaveClassName('st-icon');
+    expect(toSvgMock).toHaveBeenCalledWith({
+      height: 16,
+      width: 16,
+    });
   });
 
-  it('should render light fontawesome icon', () => {
-    wrapper.setProps({ type: 'light' });
-    expect(wrapper).toHaveClassName('fal');
+  it('should pass custom class to an icon', () => {
+    wrapper.setProps({ className: 'custom-class' });
+    expect(wrapper).toHaveClassName('custom-class');
   });
 
-  it('should render solid fontawesome icon', () => {
-    wrapper.setProps({ type: 'solid' });
-    expect(wrapper).toHaveClassName('fas');
-  });
-
-  it('should pass custom className to an icon', () => {
-    wrapper.setProps({ className: 'new-class' });
-    expect(wrapper).toHaveClassName('new-class');
+  it('should render an icon with custom color and size', () => {
+    wrapper.setProps({ color: 'red', size: 24 });
+    expect(toSvgMock).toHaveBeenCalledWith({
+      color: 'red',
+      width: 24,
+      height: 24,
+    });
   });
 });
